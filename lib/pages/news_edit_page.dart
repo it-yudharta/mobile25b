d00a8af1058140ffe632c573e5c1176dd2553fca
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2025-04-28 19:15:08 +0700

    create data

--- lib/pages/news_edit_page.dart
+++ lib/pages/news_edit_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class NewsEditPage extends StatefulWidget {
   const NewsEditPage({super.key});
@@ -8,11 +9,60 @@ class NewsEditPage extends StatefulWidget {
 }
 
 class _NewsEditPageState extends State<NewsEditPage> {
+  final _formKey = GlobalKey<FormState>();
+  String title = '';
+  String content = '';
+
+  Future save(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      final supabase = Supabase.instance.client;
+      await supabase.from('news').insert({'title': title, 'content': content});
+
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('Data berhasil disimpan')));
+
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Edit News')),
-      body: Center(child: Text('Edit News Page')),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Judul'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Masukkan judul berita';
+                }
+                return null;
+              },
+              onChanged: (value) {
+                setState(() {
+                  title = value;
+                });
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Isi Berita'),
+              onChanged: (value) {
+                setState(() {
+                  content = value;
+                });
+              },
+            ),
+            ElevatedButton(
+              onPressed: () => save(context),
+              child: const Text('Simpan'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
